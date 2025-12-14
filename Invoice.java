71485f69ecfe646d4d495923fb08223807918246
Rohan Vishwas <rohankumar@Rohans-MacBook-Air.local>
2025-12-14 20:43:41 +0530

    [IMP] SRP violation

---
+++ Invoice.java
@@ -0,0 +1,23 @@
+
+public class Invoice{
+    private Marker marker;
+    public int quantity;
+
+    public Invoice(Marker marker, int quantity){
+        this.marker = marker;
+        this.quantity = quantity;
+    }
+    public int totalBill(){
+        int total = 0;
+        total = (this.marker.price)*this.quantity;
+        return total;
+    }
+    public void  printInvoice(){
+       System.out.println("Printing invoice");
+    } 
+    public void saveToDB(){
+        System.err.println("Savig to DB");
+    }
+
+
+}
