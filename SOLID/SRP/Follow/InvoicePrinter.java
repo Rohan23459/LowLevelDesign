4d5ed3fa229e0bf61d366aba579981168702a9fb
Rohan Vishwas <rohankumar@Rohans-MacBook-Air.local>
2025-12-16 22:35:10 +0530

    [IMP] Code following Single Responsibility Principle

--- SOLID/SRP/Follow/InvoicePrinter.java
+++ SOLID/SRP/Follow/InvoicePrinter.java
@@ -1,5 +1,14 @@
 package SOLID.SRP.Follow;
 
 public class InvoicePrinter {
-    
+    private Invoice invoice;
+
+    public InvoicePrinter(Invoice invoice){
+        this.invoice = invoice;
+
+    }
+
+    public void PrintInvoice(){
+        System.out.println("printing the invoice");
+    }
 }
