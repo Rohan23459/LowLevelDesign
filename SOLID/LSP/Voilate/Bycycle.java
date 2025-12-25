54ec1e51f09224452f71cb55cd516230cb53f693
Rohan Vishwas <rohankumar@Rohans-MacBook-Air.local>
2025-12-25 22:32:36 +0530

    [IMP]Liskov substitution principle voilates code

---
+++ SOLID/LSP/Voilate/Bycycle.java
@@ -0,0 +1,39 @@
+package SOLID.LSP.Voilate;
+
+public class Bycycle implements Bike {
+     String Company;
+    boolean IsEngineOn;
+    int Speed;
+    int NumOfWheel;
+
+    public Bycycle(String company , int speed, int numOfWheel){
+        this.Company = company;
+        this.Speed = speed;
+        this.NumOfWheel = numOfWheel;
+    }
+  @Override
+    public void turnOffEngine(){
+        throw new AssertionError("Detail Message: Bicycle has no engine!");
+    }
+@Override
+    public void turnOnEngine(){
+        throw new AssertionError("Detail Message: Bicycle has no engine!");
+    }
+@Override
+    public int getNumOfWheel(){
+        System.out.printf("number of wheel are: %d%n", this.NumOfWheel);
+    return this.NumOfWheel;
+    }
+@Override
+    public void accelerate(){
+        this.Speed = this.Speed +10;
+        System.out.println("Bicycle hase accelerated");
+    }
+
+@Override
+    public void applyBreak(){
+        this.Speed = this.Speed - 10;
+        System.out.println("break has been applied");
+    }
+
+}
